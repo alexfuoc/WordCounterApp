92cfe19f3866496c0447941e849bd8f53b0f112a
alexfuoc <53452014+alexfuoc@users.noreply.github.com>
2019-11-20 11:50:11 -0500

    Added stopwords, correct output
    Added the stopwords.txt to a HashSet, correct word counting output, need to order the output next

--- src/wordcounterapp/WordCounterApp.java
+++ src/wordcounterapp/WordCounterApp.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package wordcounterapp;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner;
 
 /**
@@ -19,17 +16,37 @@ public class WordCounterApp {
 
     public static void main(String[] args) throws FileNotFoundException {
         File songFile = new File("7Rings.txt");
+        File stopwordsFile = new File("stopwords.txt");
         Scanner input = new Scanner(songFile);
+        Scanner stopwordsInput = new Scanner(stopwordsFile);
         HashMap<String,Integer> wordCounter = new HashMap<>();
+        HashSet<String> stopwords = new HashSet<>();
         
+        //Inputting the Stopwords into the set
+        while(stopwordsInput.hasNext()){
+            //clean and to lower case
+            String stopword = stopwordsInput.next().toLowerCase();
+            stopword = stopword.replaceAll("\\p{Punct}", "");
+            if(stopwords.contains(stopword));
+            else{
+                stopwords.add(stopword);
+            }
+        }
+        
+        //Inputting the Lyrics to the HashMap
         while(input.hasNext()){
-            //clean the input
+            //clean the input, remove punctuation and to Lower Case
             String next = input.next().toLowerCase();
-            if(!wordCounter.containsKey(next)){
+            next = next.replaceAll("\\p{Punct}", "");
+           
+            //catch for stopword or already in the dictionary to increment
+            //or add it to dictionary and add 1
+            if(stopwords.contains(next)) {
+            } else if(!wordCounter.containsKey(next)){
                 wordCounter.put(next, 1);
             }else{
                 wordCounter.put(next, wordCounter.get(next) + 1);
-            }
+             }
         }
         
         wordCounter.keySet().forEach((word) -> {
@@ -38,4 +55,11 @@ public class WordCounterApp {
         });
     }
     
+    /*
+    Sort the Hashmap
+    */
+    public HashMap<String, Integer> sortMap(HashMap<String, Integer> song){
+        
+        return song;
+    }
 }
