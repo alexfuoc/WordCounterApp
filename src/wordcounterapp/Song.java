d605805740a1e75d0a89af7a84aaf7e836987267
alexfuoc <53452014+alexfuoc@users.noreply.github.com>
2019-12-02 17:29:24 -0500

    Reformatted file locations
    Files are now in the src folder

--- src/wordcounterapp/Song.java
+++ src/wordcounterapp/Song.java
@@ -1,4 +1,3 @@
-
 package wordcounterapp;
 
 import java.io.*;
@@ -7,84 +6,104 @@ import java.lang.*;
 
 /*
  Song Class
-*/
+ */
 public class Song {
+
     File songFile;
+    File outputFile;
     File stopwordsFile;
+    FileWriter fr = null;
+    BufferedWriter br = null;
+    PrintWriter pr = null;
     Scanner input;
     Scanner stopwordsInput;
-    HashMap<String,Integer> songLyrics = new HashMap<>();
+    HashMap<String, Integer> songLyrics = new HashMap<>();
     HashSet<String> stopwords = new HashSet<>();
     int count;
     int totalWords = 0;
     String songTitle;
-    
-    public Song(String inputFile, String T)throws IOException{
+
+    public Song(String inputFile, String T) throws IOException {
         //creating stopwords input
         stopwordsFile = new File("stopwords.txt");
         stopwordsInput = new Scanner(stopwordsFile);
         //creating file input
         songFile = new File(inputFile);
+        outputFile = new File("output.txt");
         input = new Scanner(songFile);
         songTitle = T;
-        
+
         //Inputting the Stopwords into the set
-        while(stopwordsInput.hasNext()){
+        while (stopwordsInput.hasNext()) {
             //clean, remove punctuation and to lower case
             String stopword = stopwordsInput.next().toLowerCase();
             stopword = stopword.replaceAll("\\p{Punct}", "");
             //input stopword to set
-            if(stopwords.contains(stopword));
-            else{
+            if (stopwords.contains(stopword)); else {
                 stopwords.add(stopword);
             }
         }
-        
+
         //Inputting the Lyrics to the HashMap
-        while(input.hasNext()){
+        while (input.hasNext()) {
             //clean the input, remove punctuation and to Lower Case
             String next = input.next().toLowerCase();
             next = next.replaceAll("\\p{Punct}", "");
-           
+
             //catch for stopword or already in the dictionary to increment
             //or add it to dictionary and add 1
-            if(stopwords.contains(next)) {
-            } else if(!songLyrics.containsKey(next)){
+            if (stopwords.contains(next)) {
+            } else if (!songLyrics.containsKey(next)) {
                 songLyrics.put(next, 1);
                 totalWords++;
-            }else{
+            } else {
                 songLyrics.put(next, songLyrics.get(next) + 1);
                 totalWords++;
-             }
+            }
         }
         //Sort the Hashmap
         songLyrics = Song.sortByValue(songLyrics);
-        //printLyricsFile(songLyrics);
+        printLyricsFile(songLyrics);
         //printLyrics(songLyrics);
     }
-    
+
     /*
     Print the Lyrics to console
-    */
-    public void printLyrics(HashMap<String, Integer> song){
+     */
+    public void printLyrics(HashMap<String, Integer> song) {
         for (String word : song.keySet()) {
             count = song.get(word);
             System.out.println(count + ": " + word);
         }
     }
-    
+
     /*
     Print the Lyrics to txt file
-    */
-    public void printLyricsFile(HashMap<String, Integer> song) throws IOException{
+     */
+    public void printLyricsFile(HashMap<String, Integer> song) throws IOException {
         //create a file first    
-        PrintWriter output = new PrintWriter("output.txt");
-        output.println(songTitle);
-        for (String word : song.keySet()) {
-            count = song.get(word);
-            output.println(count + ": " + word);
+        try {
+            // to append to file, you need to initialize FileWriter using below constructor
+            fr = new FileWriter(outputFile, true);
+            br = new BufferedWriter(fr);
+            pr = new PrintWriter(br);
+            pr.println(songTitle);
+            for (String word : song.keySet()) {
+                count = song.get(word);
+                pr.println(count + ": " + word);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                pr.println();
+                pr.close();
+                br.close();
+                fr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        output.close();
     }
     
     /*
--- src/wordcounterapp/WordCounterApp.java
+++ src/wordcounterapp/WordCounterApp.java
@@ -18,15 +18,23 @@ public class WordCounterApp {
         Song[] tunSongs = new Song[numOfSongs];
         Album TUN = new Album("thank u, next", numOfSongs);
         
-        String[] inputFiles = {"imagine.txt", "needy.txt", "NASA.txt", 
-                "bloodline.txt", "fake smile.txt", "bad idea.txt", 
-                "make up.txt", "ghostin.txt", "in my head.txt", "7 rings.txt", 
-                "thank u, next.txt", "break up with your girlfriend, im bored.txt"};
+        String[] inputFiles = {"src\\thank u, next\\imagine.txt", 
+            "src\\thank u, next\\needy.txt", 
+            "src\\thank u, next\\NASA.txt", 
+            "src\\thank u, next\\bloodline.txt", 
+            "src\\thank u, next\\fake smile.txt", 
+            "src\\thank u, next\\bad idea.txt",
+            "src\\thank u, next\\make up.txt", 
+            "src\\thank u, next\\ghostin.txt", 
+            "src\\thank u, next\\in my head.txt", 
+            "src\\thank u, next\\7 rings.txt",
+            "src\\thank u, next\\thank u, next.txt", 
+            "src\\thank u, next\\break up with your girlfriend, im bored.txt"};
         
         String[] songNames = {"imagine", "needy", "NASA", 
                 "bloodline", "fake smile", "bad idea", "make up", "ghostin", 
-                "in my head", "7 rings", "thank u, next.txt", 
-                "break up with your girlfriend, im bored.txt"};
+                "in my head", "7 rings", "thank u, next", 
+                "break up with your girlfriend, im bored"};
         
         for(int i = 0; i < songNames.length; i++){
             tunSongs[i] = new Song(inputFiles[i], songNames[i]);
